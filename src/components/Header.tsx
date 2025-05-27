1a4e23069873a9e9819bc053ce3c2fda2714f8bd
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-27 11:53:06 -0300

    feat: adiciona componente Header com navegação

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header>
+      <div className="logo">
+        <h1 className="logo-text"><span>Mundo</span>Pet</h1>
+      </div>
+      <i 
+        className="fa fa-bars menu" 
+        id="menuToggle"
+        onClick={() => setMenuOpen(!menuOpen)}
+      />
+      <ul className="nav" id="menuItems" style={{ display: menuOpen ? 'block' : '' }}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/sobre">Sobre</Link></li>
+        <li><Link to="/criar-post">Criar post</Link></li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
