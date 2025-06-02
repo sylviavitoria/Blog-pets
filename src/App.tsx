0c422611a695d0ab3c17392c201e56bbea5cf401
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-06-02 10:14:55 -0300

    refactor: remove código desnecessário e organiza a estrutura do componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter,  Routes, Route} from 'react-router-dom';
+import Header from 'c:/estagio/blog-pets/blog-pets/src/components/Header';
+import Footer from 'c:/estagio/blog-pets/blog-pets/src/components/Footer';
+import Home from 'c:/estagio/blog-pets/blog-pets/src/pages/Home';
+import CriarPost from './components/CriarPost';
+import Sobre from './components/Sobre';
+// import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/criar-post" element={<CriarPost />} />
+          <Route path="/sobre" element={<Sobre />} />
+        </Routes>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
